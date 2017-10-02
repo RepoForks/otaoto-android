c5819e8a65be173a89a0d0591fd1b7e42ebb3702
Russell <russhwolf@gmail.com>
2017-10-01 22:00:51 -0400

    Add espresso intent test for confirm activity share button

--- app/src/androidTest/java/co/otaoto/ui/confirm/ConfirmActivityTest.kt
+++ app/src/androidTest/java/co/otaoto/ui/confirm/ConfirmActivityTest.kt
@@ -1,17 +1,25 @@
 package co.otaoto.ui.confirm
 
+import android.app.Instrumentation
+import android.content.Intent.*
 import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.intent.Intents.intended
+import android.support.test.espresso.intent.Intents.intending
+import android.support.test.espresso.intent.matcher.BundleMatchers.hasEntry
+import android.support.test.espresso.intent.matcher.IntentMatchers.*
+import android.support.test.espresso.intent.rule.IntentsTestRule
 import android.support.test.espresso.matcher.ViewMatchers.*
-import android.support.test.rule.ActivityTestRule
 import android.support.transition.TransitionManager
 import android.widget.ToggleButton
 import co.otaoto.R
+import org.hamcrest.Matchers.allOf
 import org.hamcrest.Matchers.instanceOf
 import org.junit.Rule
 import org.junit.Test
+
 
 class ConfirmActivityTest {
     companion object {
@@ -23,7 +31,7 @@ class ConfirmActivityTest {
 
     @Rule
     @JvmField
-    val activityTestRule = object : ActivityTestRule<ConfirmActivity>(ConfirmActivity::class.java) {
+    val activityTestRule = object : IntentsTestRule<ConfirmActivity>(ConfirmActivity::class.java) {
         override fun getActivityIntent() = ConfirmActivity.newIntent(InstrumentationRegistry.getTargetContext(), SECRET, SLUG, KEY)
     }
 
@@ -31,7 +39,21 @@ class ConfirmActivityTest {
     fun smokeTest() {
         val linkText = onView(hasLinks())
         linkText.check(matches(withText(URL)))
-        // TODO add check for chooser window
+
+        val result = Instrumentation.ActivityResult(0, null)
+        intending(hasAction(ACTION_CHOOSER)).respondWith(result)
+        linkText.perform(click())
+        intended(allOf(
+                hasAction(ACTION_CHOOSER),
+                hasExtras(allOf(
+                        hasEntry(EXTRA_TITLE, activityTestRule.activity.getString(R.string.label_share_url)),
+                        hasEntry(EXTRA_INTENT, allOf(
+                                hasAction(ACTION_SEND),
+                                hasType("text/*"),
+                                hasExtra(EXTRA_TEXT, URL)
+                        ))
+                ))
+        ))
 
         val toggleButton = onView(instanceOf(ToggleButton::class.java))
         val secretText = onView(withId(R.id.confirm_secret_text))
